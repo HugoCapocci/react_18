21765b45507504d6f0f4cae689c633c69be7299f
Hugo CAPOCCI <h.capocci@gmail.com>
2022-12-05 21:59:38 +0100

    Implement example for useTransition hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const SlowUI = ({ value } : { value: number }) => (
+  <>
+    {Array(value)
+      .fill(1)
+      .map((_, index) => (
+        <span key={index}>{value - index} </span>
+      ))}
+  </>
+);
+
 function App() {
+  const [value, setValue] = React.useState(0);
+  const [value2, setValue2] = React.useState(100);
+  const [isPending, startTransition] = React.useTransition();
+
+  const handleClick = () => {
+    setValue(value + 1);
+    // low priority rending
+    startTransition(() => setValue2(value2 * 2));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleClick}>{value}</button>
+      <div
+        style={{
+          opacity: isPending ? 0.5 : 1,
+        }}
+      >
+        <SlowUI value={value2} />
+      </div>
+    </>
   );
 }
 
